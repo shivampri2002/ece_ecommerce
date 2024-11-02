da878a103cc6a524cd565e5765fb0a3dc7e5bbd7
shivam <shivprishi@gmail.com>
2024-11-03 00:22:08 +0530

    stripe payment added

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,16 +1,38 @@
 import React, { useRef } from 'react';
 import Link from 'next/link';
-
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
 import { TiDeleteOutline } from 'react-icons/ti';
 import toast from 'react-hot-toast';
 
-import { useStateContext } from "../context/StateContext";
-import { urlFor } from "../lib/client";
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } = useStateContext();
+
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
+  
 
   return (
     <div className="cart-wrapper" ref={cartRef}>
@@ -86,7 +108,7 @@ const Cart = () => {
         )}
       </div>
     </div>
-  );
+  )
 }
 
 export default Cart
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,23 @@
 import React from 'react';
 
+import { client } from '../lib/client';
 import { Product, FooterBanner, HeroBanner } from '../components';
 
-import { client } from '../lib/client';
+const Home = ({ products, bannerData }) => (
+    <div>
+        <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+        <div className="products-heading">
+            <h2>Best Seller Products</h2>
+            <p>speaker There are many variations passages</p>
+        </div>
 
-const Home = ({ products, bannerData }) => {
-    return (
-        <div>
-            <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
-            <div className="products-heading">
-                <h2>Best Seller Products</h2>
-                <p>speaker There are many variations passages</p>
-            </div>
+        <div className="products-container">
+            {products?.map((product) => <Product key={product._id} product={product} />)}
+        </div>
 
-            <div className="products-container">
-                {products?.map((product) => <Product key={product._id} product={product} />)}
-            </div>
-
-            <FooterBanner footerBanner={bannerData && bannerData[0]} />
-        </div >
-    );
-};
+        <FooterBanner footerBanner={bannerData && bannerData[0]} />
+    </div>
+);
 
 export const getServerSideProps = async () => {
     const query = '*[_type == "product"]';
@@ -30,10 +27,8 @@ export const getServerSideProps = async () => {
     const bannerData = await client.fetch(bannerQuery);
 
     return {
-        props: {
-            products, bannerData
-        }
-    };
-};
+        props: { products, bannerData }
+    }
+}
 
 export default Home;
